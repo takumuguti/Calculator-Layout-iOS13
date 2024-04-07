c81696624ece434444019ebc14eb69cae0a31690
Takudzwanashe Dylan Muguti <90533439+takumuguti@users.noreply.github.com>
2024-04-08 09:38:10 +1000

    Add to GitHub

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UserOutput: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func percentageButton(_ sender: Any) {
+        setUserInput(with: "%")
+    }
+    
+    @IBAction func renameThisButton(_ sender: Any) {
+        setUserInput(with: "😅")
+    }
+    @IBAction func clearButton(_ sender: Any) {
+        UserOutput.text = "0"
+    }
+    @IBAction func dividedButton(_ sender: Any) {
+        setUserInput(with: "/")
+    }
+    @IBAction func seven(_ sender: Any) {
+        setUserInput(with: "7")
+    }
+    @IBAction func eight(_ sender: Any) {
+        setUserInput(with: "8")
+    }
+    @IBAction func nine(_ sender: Any) {
+        setUserInput(with: "8")
+    }
+    @IBAction func multiply(_ sender: Any) {
+        setUserInput(with: "x")
+    }
+    @IBAction func four(_ sender: Any) {
+        setUserInput(with: "4")
+    }
+    @IBAction func five(_ sender: Any) {
+        setUserInput(with: "5")
+    }
+    @IBAction func six(_ sender: Any) {
+        setUserInput(with: "6")
+    }
+    @IBAction func minus(_ sender: Any) {
+        setUserInput(with: "-")
+    }
+    @IBAction func one(_ sender: Any) {
+        setUserInput(with: "1")
+    }
+    @IBAction func two(_ sender: Any) {
+        setUserInput(with: "2")
+    }
+    @IBAction func three(_ sender: Any) {
+        setUserInput(with: "3")
+    }
+    @IBAction func plus(_ sender: Any) {
+        setUserInput(with: "+")
+    }
+    @IBAction func zero(_ sender: Any) {
+        setUserInput(with: "0")
+    }
+    @IBAction func comma(_ sender: Any) {
+        setUserInput(with: ".")
+    }
+    
+    @IBAction func equalButton(_ sender: Any) {
+        UserOutput.text = evaluateExpression()
+    }
+    
+    private func setUserInput(with output: String){
+        if UserOutput.text == "0" {
+            UserOutput.text = output
+        } else {
+            UserOutput.text! += output
+        }
+    }
+    
+    private func evaluateExpression() -> String {
+        return "To be implemented..."
+    }
+    
+    
 
 }
 
